929b93dec507922f936820834b93229195c678fc
Rudy Gomez <gomez.rudy@gmail.com>
2020-02-29 02:04:20 -0800

    Connect combined data source

--- HackerNews-Swift-Combine/NewsTableViewController.swift
+++ HackerNews-Swift-Combine/NewsTableViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Combine
 
 class NewsTableViewController: UITableViewController {
+  
+  let newsApi = API()
+  
+  var subscriptions = Set<AnyCancellable>()
+  
+  var newsStories = [Story]() {
+    didSet {
+      DispatchQueue.main.async { [weak self] in
+        self?.tableView.reloadData()
+      }
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    newsApi.latestStories()
+      .sink(receiveCompletion: { print($0) }, receiveValue: {
+        self.newsStories = $0
+      })
+      .store(in: &subscriptions)
   }
 
-
+  // MARK: - UITabeViewDataSource Methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.newsStories.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "newscell", for: indexPath)
+    cell.textLabel?.text = self.newsStories[indexPath.row].title
+    cell.detailTextLabel?.text = "By \(self.newsStories[indexPath.row].by)"
+    
+    return cell
+  }
+  
 }
 
