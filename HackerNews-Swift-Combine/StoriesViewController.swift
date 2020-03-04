e25ac87cdecb7c235b7b385c28b55e4dbda03b2e
Rudy Gomez <gomez.rudy@gmail.com>
2020-03-04 02:16:48 -0800

    Connect story table view cell

--- HackerNews-Swift-Combine/StoriesViewController.swift
+++ HackerNews-Swift-Combine/StoriesViewController.swift
@@ -22,6 +22,13 @@ class StoriesViewController: UITableViewController {
       self.tableView.reloadData()
     }
   }
+  
+  private static var timeFormatter: DateFormatter = {
+     let formatter = DateFormatter()
+     formatter.dateStyle = .none
+     formatter.timeStyle = .short
+     return formatter
+   }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -41,11 +48,20 @@ class StoriesViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "newscell", for: indexPath)
-    cell.textLabel?.text = self.newsStories[indexPath.row].title
-    cell.detailTextLabel?.text = "By \(self.newsStories[indexPath.row].by)"
+    let cell = tableView.dequeueReusableCell(withIdentifier: "storycell", for: indexPath)
+    guard let storyCell = cell as? StoryTableViewCell else { return cell }
     
-    return cell
+    let date = Date(timeIntervalSince1970: self.newsStories[indexPath.row].time)
+    storyCell.timeLabel?.text = StoriesViewController.timeFormatter.string(from: date)
+    storyCell.titleLabel?.text = self.newsStories[indexPath.row].title
+    storyCell.subtitleLabel?.text = "By \(self.newsStories[indexPath.row].by)"
+    storyCell.linkButton?.setTitle(self.newsStories[indexPath.row].url, for: .normal)
+    
+    return storyCell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      return 235
   }
   
 }
---
+++ HackerNews-Swift-Combine/View/StoryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StoryTableViewCell.swift
+//  HackerNews-Swift-Combine
+//
+//  Created by Rudy Gomez on 3/4/20.
+//  Copyright © 2020 JRudy Gaming. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var subtitleLabel: UILabel!
+  @IBOutlet weak var linkButton: UIButton!
+  
+}
